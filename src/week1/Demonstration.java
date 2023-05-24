15b1a3205da7f5a37d63ad48d270fb11197c3722
Sufyan <thelast.taoued@gmail.com>
2023-05-24 13:13:53 -0400

    creating methode for dividing without operator

---
+++ src/week1/Demonstration.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class Demonstration {
+    public static void main(String[] args) {
+
+        DivisionWithoutOperator divid = new DivisionWithoutOperator();
+
+        System.out.println("the reualt is  " + divid.dividWithoutOeration(13,-2));
+
+    }
+
+
+}
--- src/week1/DivisionWithoutOperator.java
+++ src/week1/DivisionWithoutOperator.java
@@ -8,6 +8,33 @@ public class DivisionWithoutOperator {
 
     // Solution Below
 
+    public int dividWithoutOeration(int number1 ,int number2){
+
+        int resualt = 0;
+        //sign variable is for converting numbers
+        int sign = 1;
+        if (number1 * number2 < 0 ) {
+            sign = -1;
+        }
+        // convert the number to positive if the numbers is negative
+        number1 = Math.abs(number1);
+        number2 = Math.abs(number2);
+
+        //we can not divid by 0
+        if ( number2 == 0){
+            System.out.println("can't divid by 0");
+            System.exit(-1);
+        }
+
+        while (number1 >= number2){
+            number1 = number1 - number2;
+            resualt++;
+        }
+        System.out.println("the remainder is  " + number1);
+        return  resualt * sign;
+
+    }
+
 
 
 
