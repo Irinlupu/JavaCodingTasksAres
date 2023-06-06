2266acac4d9b67bafd0f69554e349b7e3cbda490
Irinlupu <irinlupu@gmail.com>
2023-06-06 13:35:14 -0400

    Updated with Solutions week3: PrimeNumber, ReverseNegativeNumber

---
+++ src/irina/week3/PrimeNumber.java
@@ -0,0 +1,27 @@
+package irina.week3;
+
+public class PrimeNumber {
+    /*
+    Numbers -- Prime Number
+        Write a method that can check if a number is prime or not.
+     */
+    public static void main(String[] args) {
+        isPrime(28);
+    }
+
+    public static void isPrime(int num){
+        boolean isPrime = true;
+
+        for (int i = 2; i < num; i++) {
+            if(num % i == 0 ){
+                isPrime = false;
+
+            }
+        }
+
+        System.out.println(num + " isPrime = " + isPrime);
+
+
+
+    }
+}
