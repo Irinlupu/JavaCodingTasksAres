7b8cb52e640b630da85c326cd70afdcf8d4312ea
Cydeo-Huy <hhuylle@gmail.com>
2023-06-07 10:51:50 -0400

    added 2 java coding task for week3

---
+++ src/HuyL/week3/PrimeNumber.java
@@ -0,0 +1,19 @@
+package HuyL.week3;
+
+public class PrimeNumber {
+
+    public static boolean isPrimeNumber(int num){
+        if (num<=1){ // to check if num less than or equal to 1
+            return false; // return false if number less than or equal to 1
+        }else {
+            for (int i = 2; i < num; i++) { // to check from 2 to num-1
+                if (num%i==0){ // checks if nums are even number
+                    return false;
+                }
+            }
+        }
+        return true; // returns true
+    }
+
+}
+//Write a method that can check if a number is prime or not.
---
+++ src/HuyL/week3/ReverseNegativeNumber.java
@@ -0,0 +1,24 @@
+package HuyL.week3;
+
+public class ReverseNegativeNumber {
+
+    public static int reverseNegativeNum(int num) {
+
+        int reverse = 0;
+        if (num>=0){ // checks if number is greater than or equal to 0
+            System.out.println("Number must be negative");
+            System.exit(1);
+        }else { // to checks if number less than or equal to 0
+
+            int lastNum; // assign lastNum variable
+            while (num != 0){ // uses While loop with condition is number is not equal to 0
+                lastNum = num%10; // to store last digit to lastNum variable
+                reverse = reverse*10+lastNum; // to store last digit to reverse variable
+                num = num / 10; // to eliminate last digit
+            }
+        }
+        return reverse; // returns reverse version number that number we are given
+    }
+}
+//Write a return method that can reverse digits of a negative number and return it as int
+//input: -35  output: -53
