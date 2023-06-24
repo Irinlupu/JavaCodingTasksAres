d03445224a6aac2d6943dc16e598845a6257a772
anhlushko <anhlushko@gmail.com>
2023-06-24 15:04:00 -0400

    SumOfAllDigits.java updated

--- src/andriy/week6/SumOfAllDigits.java
+++ src/andriy/week6/SumOfAllDigits.java
@@ -1,24 +1,41 @@
 package andriy.week6;
 
-import java.util.Arrays;
+
 
 public class SumOfAllDigits {
 
     public static int sumOfAllDigits(String str) {
-        int sumofDigits = 0;
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            if (Character.isDigit(ch)) {
-                sumofDigits += Integer.parseInt(str.charAt(i)+"");
+        int sumOfDigits = 0;
+        String newStr = "";
+        String[] strArr = str.split(" ");
+        for (String eachElement : strArr) {
+            String strOfNum = "";
+            for (int i = 0; i < eachElement.length(); i++) {
+                if (Character.isDigit(eachElement.charAt(i))) {
+                    strOfNum += eachElement.charAt(i);
+                }
             }
+            newStr += strOfNum + " ";
         }
-        return sumofDigits;
+        System.out.println(newStr);
+
+        for (String each : newStr.split(" ")) {
+            if (each.equals("")){
+                continue;
+            }
+            sumOfDigits+=Integer.parseInt(each);
+        }
+        return sumOfDigits;
     }
 
     public static void main(String[] args) {
-        System.out.println(sumOfAllDigits("12 java 5 apple 3"));
+
+        String str = "120 java 5 apple 3";
+        System.out.println("Sum of digits = " + sumOfAllDigits(str));
     }
-}
+
+    }
+
 /*
 String_SumOfAllDigits
 Write a method that can return the sum of the digits in a string
