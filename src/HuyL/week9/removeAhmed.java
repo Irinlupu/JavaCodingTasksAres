3ab6c8d1c5c5069a587f9a2183cd4823c4d65508
Cydeo-Huy <hhuylle@gmail.com>
2023-07-29 22:59:34 -0400

    updated removedAhmed of week9

--- src/HuyL/week9/removeAhmed.java
+++ src/HuyL/week9/removeAhmed.java
@@ -13,6 +13,22 @@ public class removeAhmed {
         listNamesWithoutAhmed.removeIf(p -> p.contains("Ahmed"));
 
         System.out.println(listNamesWithoutAhmed);
+
+        /*
+        ArrayList<String> listOfNames = new ArrayList<>(Arrays.asList("Ahmed", "John", "Eric", "Ahmed", "Aaron", "Andrew", "Kelly"));
+
+        ArrayList<String> withoutNameAhmed = new ArrayList<>();
+
+        for (String each : listOfNames) {
+            if (each.equals("Ahmed")){
+                continue;
+            }
+            withoutNameAhmed.add(each);
+        }
+
+        System.out.println(withoutNameAhmed);
+
+         */
     }
 
 }
